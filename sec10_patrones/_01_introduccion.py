e741d2f90cc2243c3f83b7019ad693b0c0caae97
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2024-02-19 17:38:38 -0600

    Se agrega seccion sobre patrones de disenio

---
+++ sec10_patrones/_01_introduccion.py
@@ -0,0 +1,17 @@
+"""INTRODUCCIÓN
+
+Los patrones de diseño son 'plantillas' reutilizables para resolver
+problemas generales de software.
+
+Dependiendo del fin se pueden dividir en:
+- Creacionales (creational patters)
+    Simplifican la creación de objetos.
+- Estructurales (structural patterns)
+    Mejoran la composición y la relación entre objetos
+- De comportamiento
+    Optimizan la comunicación y responsabilidades
+    entre objetos.
+
+Como beneficio, aportan soluciones generales probadas y mejora la
+comunicación entre desarrolladores.
+"""
