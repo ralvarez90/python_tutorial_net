830b1e5c2688789c5941b5f71696fe04e6833883
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2024-04-12 15:15:40 -0600

    Leccion sobre monkey patching

---
+++ sec03_avanzado/_25_monkey_patching.py
@@ -0,0 +1,7 @@
+"""MONKEY PATCHING
+
+Técnica que permite modificar o extender el comportamiento de las
+clases, métodos o funciones en tiempo de ejecución.
+
+Básicamente consiste en cambiar o extender 
+"""
