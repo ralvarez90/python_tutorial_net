f77dfe080352c8dfc765405b5662345cd5e0db99
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2024-05-08 16:58:38 -0600

    Se completa leccion sobre iter.

--- sec03_avanzado/_36_iter_function.py
+++ sec03_avanzado/_36_iter_function.py
@@ -8,6 +8,16 @@ un objeto iterable o una secuencia.
 
 Primero buscará el método __iter__ del objeto argumento
 y si no existe buscará el __getitem__.
+
+En el ejemplo 02, debido a que Counter implementa el 
+método __getitem__() que devuelve un elemento basado en un 
+índice, es una secuencia.
+
+Ahora, puedes usar la función iter() para obtener el 
+iterador del contador, ver ejemplo 02.
+
+En el ejemplo 03, se agrega la clase CounterIterator a la clase
+Counter implementando el protocolo Iterable.
 """
 
 
@@ -36,9 +46,47 @@ def showExample02():
                 self.current += 1
                 return self.current
 
+    # get instance
+    counter = Counter()
+    iterator = iter(counter)
+    print(f'Type(iterator) -> {type(iterator)}')
+
+
+def showExample03():
+
+    class Counter:
+        def __init__(self) -> None:
+            self.current = 0
+
+        def __getitem__(self, index: int):
+            if isinstance(index, int):
+                self.current += 1
+                return self.current
+
+        def __iter__(self):
+            return CounterIterator(counter=self)
+
+    class CounterIterator:
+        def __init__(self, counter: Counter) -> None:
+            self.__counter = counter
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            self.__counter.current += 1
+            return self.__counter.current
+
+    # run ejemplo
+    counter = Counter()
+    iterator = iter(counter)
+    print(type(iterator))
+
 
 def main():
     showExample01()
+    showExample02()
+    showExample03()
 
 
 if __name__ == '__main__':
---
+++ sec03_avanzado/_37_segunda_forma_iter_function.py
@@ -0,0 +1,8 @@
+"""SEGUNDA FORMA DE FUNCIÓN iter
+
+Existe una segunda forma de implementación de la función iter, con
+la firma:
+iter(callable, sentinel)
+
+Esta forma permite invocar un invoca
+"""
